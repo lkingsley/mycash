ab7dc5ba3603a0eeb4f0617be573a35434665cd7
lkingsley <lkingsley00@gmail.com>
2025-05-29 10:42:35 -0400

    template changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy 
-from sqlalchemy import select
+from sqlalchemy import select, Date
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
@@ -12,30 +12,36 @@ db = SQLAlchemy(model_class=Base)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db.init_app(app)
 
-
 class Transact(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     amount: Mapped[str] = mapped_column(nullable=False)
     method: Mapped[str] = mapped_column(nullable=False)
     date: Mapped[str] = mapped_column(nullable=False)
-
+    type: Mapped[str] = mapped_column(nullable=False)
 
 with app.app_context():
     db.create_all()
 
 @app.route("/")
 def incomes():
+    request.args.get("")
     transactions = db.session.execute(select(Transact)).scalars()
     return render_template("index.html", transactions=transactions)
 
-@app.route("/", methods=["POST"])
+@app.route("/transactions", methods=["POST"])
 def transactions():
-    type = request.form["type"]
+    transaction_type = request.form["type"]
     amount = request.form["amount"]
     method = request.form["method"]
     date = request.form["date"]
+    
+    #TODO:validate fields
+    transaction = Transact(amount=amount,method=method,date=date, type=transaction_type)
+    try:
+        db.session.add(transaction)
+        db.session.commit()
+    except Exception:
+        #TODO: Send error message
+        pass
 
-    transaction = Transact(amount=amount,method=method,date=date)
-    db.session.add(transaction)
-    db.session.commit()
     return redirect("/")
