1f35bb998f69332cbbdfd42c7366a9a92977ba34
lkingsley <lkingsley00@gmail.com>
2025-05-28 20:55:16 -0400

    db and model mods

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy 
-from model.income import Income
-from model.spend import Spend
+from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-db = SQLAlchemy(app)
+db.init_app(app)
+
+
+class Transact(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[str] = mapped_column(nullable=False)
+    method: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[str] = mapped_column(nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def incomes():
-    transaction1 = Income(200, "cash", "2025-05-28")
-    transaction2 = Income(200, "cash", "2025-05-28")
-    return render_template("index.html", transactions=[transaction1,transaction2])
+    transactions = db.session.execute(select(Transact)).scalars()
+    return render_template("index.html", transactions=transactions)
 
 @app.route("/", methods=["POST"])
 def transactions():
@@ -19,9 +34,8 @@ def transactions():
     amount = request.form["amount"]
     method = request.form["method"]
     date = request.form["date"]
-    if type == "income":
-        transaction = Income(amount, method, date)
-    else:
-        transaction = Spend(amount, method, date)
-    print(transaction)
-    return str(transaction)
+
+    transaction = Transact(amount=amount,method=method,date=date)
+    db.session.add(transaction)
+    db.session.commit()
+    return redirect("/")
--- model/spend.py
+++ model/spend.py
@@ -2,6 +2,6 @@ from model.transaction import Transaction
 
 class Spend(Transaction):
     def __init__(self, amount, method, date):
-        super.__init__(amount, method, date)
+        super().__init__(amount, method, date)
     def __str__(self):
         return f"Spend {{amount={self.amount}, method={self.method}, date={self.date}}}"
