c90e5a352fb9aba96be434b8dc81ddb8ba124305
lkingsley <lkingsley00@gmail.com>
2025-05-30 12:30:46 -0400

    delete files

--- model/income.py
+++
@@ -1,7 +0,0 @@
-from model.transaction import Transaction
-
-class Income(Transaction):
-    def __init__(self, amount, method, date):
-        super().__init__(amount, method, date)
-    def __str__(self):
-        return f"Income {{amount={self.amount}, method={self.method}, date={self.date}}}"
