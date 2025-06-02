066588e867ea2b8bf47c432d5431e9a3ca01895c
lkingsley <lkingsley00@gmail.com>
2025-06-02 17:56:14 -0400

    add (year, month) inputs on template

--- app.py
+++ app.py
@@ -3,10 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc, select
 from model.base import Base
 from model.transaction import Transaction
+import datetime
 
 INCOME = "income"
 SPEND = "spend"
 DATABASE_URI = "sqlite:///test.db"
+
+class Month:
+    def __init__(self, name, id):
+        self.id = id
+        self.name = name
+months = []
+months.append(Month(name="january",id=1))
+months.append(Month(name="february",id=2))
+months.append(Month(name="march",id=3))
+months.append(Month(name="april",id=4))
+months.append(Month(name="may",id=5))
+months.append(Month(name="june",id=6))
+months.append(Month(name="july",id=7))
+months.append(Month(name="august",id=8))
+months.append(Month(name="septmeber",id=9))
+months.append(Month(name="october",id=10))
+months.append(Month(name="november",id=11))
+months.append(Month(name="december",id=12))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
@@ -22,15 +41,21 @@ def incomes():
     total_income = 0
     total_spend = 0
     transactions = []
+    actual_year = datetime.datetime.now().year
+    years = [actual_year]
     query_result = db.session.execute(select(Transaction).order_by(desc(Transaction.date))).scalars()
     
+    for i in range(9):
+        actual_year -= 1
+        years.append(actual_year)
+
     for i in query_result:
         transactions.append(i)
         if i.type == INCOME:
             total_income += i.amount
         else:
             total_spend += i.amount
-    return render_template("index.html", transactions=transactions, total_income=total_income, total_spend=total_spend)
+    return render_template("index.html", transactions=transactions, total_income=total_income, total_spend=total_spend, months=months, years=years)
 
 @app.route("/transactions", methods=["POST"])
 def transactions():
