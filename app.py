75be87f688b9632223c352b0109b4baa54017738
lkingsley <lkingsley00@gmail.com>
2025-06-04 10:44:42 -0400

    round total_income and total_spend to two decimals

--- app.py
+++ app.py
@@ -9,24 +9,6 @@ INCOME = "income"
 SPEND = "spend"
 DATABASE_URI = "sqlite:///test.db"
 
-class Month:
-    def __init__(self, name, id):
-        self.id = id
-        self.name = name
-months = []
-months.append(Month(name="january",id=1))
-months.append(Month(name="february",id=2))
-months.append(Month(name="march",id=3))
-months.append(Month(name="april",id=4))
-months.append(Month(name="may",id=5))
-months.append(Month(name="june",id=6))
-months.append(Month(name="july",id=7))
-months.append(Month(name="august",id=8))
-months.append(Month(name="septmeber",id=9))
-months.append(Month(name="october",id=10))
-months.append(Month(name="november",id=11))
-months.append(Month(name="december",id=12))
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 db = SQLAlchemy(model_class=Base)
@@ -45,7 +27,7 @@ def list_transactions():
     years = [actual_year]
     query_result = db.session.execute(select(Transaction)
         .order_by(desc(Transaction.date))).scalars()
-    
+    #TODO: find alternative
     for i in range(9):
         actual_year -= 1
         years.append(actual_year)
@@ -58,8 +40,8 @@ def list_transactions():
             total_spend += i.amount
 
     return render_template(
-        "index.html", transactions=transactions, total_income=total_income,
-        total_spend=total_spend, months=months, years=years
+        "index.html", transactions=transactions, total_income=round(total_income, 2),
+        total_spend=round(total_spend, 2), years=years
     )
 
 @app.route("/login", methods=["GET", "POST"])
